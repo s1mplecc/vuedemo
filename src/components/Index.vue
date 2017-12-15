59ec0d7a825b311a36e72967527bedd51afbae27
s1mple <798342418@qq.com>
2017-12-15 13:41:29 +0800

    #002 - zhangzhen - add footer component

---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg }}</h2>
+    <footer-nav></footer-nav>
+  </div>
+</template>
+
+<script>
+  import FooterNav from './public/Footer'
+
+  export default {
+    name: 'Index',
+    data() {
+      return {
+        msg: '欢迎来到人员管理系统'
+      }
+    },
+    components: {FooterNav}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+    color: #42b983;
+  }
+</style>
