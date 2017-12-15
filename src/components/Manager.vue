4feb00c5429f03a47a46b82848bd9213f43e406d
s1mple <798342418@qq.com>
2017-12-15 15:23:31 +0800

    #002 - zhangzhen - change css code

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="manager">
     <button v-on:click="add">新增</button>
-    <form class="input-area" v-show="showAdd">
-      <input type="text" placeholder="请输入人员姓名" required="required" v-model="nameValue"/>
-      <button type="submit" v-on:click="addName">确定</button>
-    </form>
+
     <table WIDTH=200 CellSpacing=8>
       <tr>
         <th>姓名</th>
@@ -15,6 +12,13 @@
         <td v-bind:id="index"><span v-on:click="del">删除</span></td>
       </tr>
     </table>
+
+    <div class="wrap">
+      <form class="input-area" v-show="showAdd">
+        <input type="text" placeholder="请输入人员姓名" required="required" v-model="nameValue"/>
+        <button type="submit" v-on:click="addName">确定</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -35,6 +39,8 @@
       addName() {
         if (this.nameValue.trim() !== '') {
           this.peoples.push({'name': this.nameValue})
+          this.showAdd = false
+          this.nameValue=undefined
         }
       },
       del(e) {
@@ -47,8 +53,25 @@
 <style scoped>
   button {
     font-weight: normal;
-    color: #42b983;
+    color: aqua;
     background: lightblue;
+    display: inline-block;
+    *display: inline;
+    vertical-align: baseline;
+    margin: 0 2px;
+    outline: none;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    font: 14px/100% Arial, Helvetica, sans-serif;
+    padding: .4em 2em .55em;
+    text-shadow: 0 1px 1px rgba(0,0,0,.3);
+    -webkit-border-radius: .5em;
+    -moz-border-radius: .5em;
+    border-radius: .5em;
+    -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    -moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    box-shadow: 0 1px 2px rgba(0,0,0,.2);
   }
 
   span {
