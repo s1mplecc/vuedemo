f141b202428ec39900668f56a50d1badddad593f
s1mple <798342418@qq.com>
2017-12-15 14:09:45 +0800

    #002 - zhangzhen - add manager html

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="manager">
-      <button>新增</button>
+  <div class="manager">
+    <button>新增</button>
+    <div class="input-area">
+      <input type="text" placeholder="请输入人员姓名"/>
+      <button>确定</button>
     </div>
+    <table>
+      <tr>
+        <th>姓名</th>
+        <th>操作</th>
+      </tr>
+      <tr>
+        <td>张三</td>
+        <td><span>编辑</span> <span>删除</span></td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "manager"
-    }
+  export default {
+    name: "manager"
+  }
 </script>
 
 <style scoped>
-
+  button {
+    font-weight: normal;
+    color: #42b983;
+    background: lightblue;
+  }
 </style>
