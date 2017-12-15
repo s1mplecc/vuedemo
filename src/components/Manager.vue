2be1f6720cdd08d917e371530ebf444b84f6d61d
s1mple <798342418@qq.com>
2017-12-15 13:55:34 +0800

    #002 - zhangzhen - add manager component

---
+++ src/components/Manager.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="manager">
+      <button>新增</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "manager"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Manager from '@/components/Manager'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },{
+      path: '/manager',
+      name: 'Manager',
+      component: Manager
     }
   ]
 })
