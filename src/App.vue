c889c635d290c984eeb35f066c549235d5e8dd99
s1mple <798342418@qq.com>
2017-12-15 13:48:49 +0800

    #002 - zhangzhen - add footer component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <footer-nav></footer-nav>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import FooterNav from './components/public/Footer'
+
+  export default {
+    name: 'app',
+    components: {FooterNav}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="hello">
     <h2>{{ msg }}</h2>
-    <footer-nav></footer-nav>
   </div>
 </template>
 
 <script>
-  import FooterNav from './public/Footer'
-
   export default {
     name: 'Index',
     data() {
       return {
         msg: '欢迎来到人员管理系统'
       }
-    },
-    components: {FooterNav}
+    }
   }
 </script>
 
