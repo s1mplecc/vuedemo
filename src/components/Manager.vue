7d5340b67f6bd91c5e0a474766856b5289d59072
s1mple <798342418@qq.com>
2017-12-15 15:11:27 +0800

    #002 - zhangzhen - change css code

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -5,7 +5,7 @@
       <input type="text" placeholder="请输入人员姓名" required="required" v-model="nameValue"/>
       <button type="submit" v-on:click="addName">确定</button>
     </form>
-    <table>
+    <table WIDTH=200 CellSpacing=8>
       <tr>
         <th>姓名</th>
         <th>操作</th>
@@ -37,8 +37,8 @@
           this.peoples.push({'name': this.nameValue})
         }
       },
-      del(e){
-        this.peoples.splice(e.target.offsetParent.id,1)
+      del(e) {
+        this.peoples.splice(e.target.offsetParent.id, 1)
       }
     }
   }
@@ -53,5 +53,13 @@
 
   span {
     color: aqua;
+  }
+
+  table {
+    margin: 0 auto;
+  }
+
+  td {
+    text-align: center;
   }
 </style>
